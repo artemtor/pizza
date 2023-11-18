5b4d027f3c3592d293ded4f18433279a11ed1734
artemtor <151242343+artemtor@users.noreply.github.com>
2023-11-18 10:20:34 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,66 @@
+import kotlin.system.exitProcess
+fun main() {
+    val pizzaPeter = PizzaPeter(
+        175.0, 241.5,
+        167.5, 215.0
+    )
+    val pizzaMoscow = PizzaMoscow(
+        215.0, 250.5,
+        180.5, 240.0
+    )
+    val pizzaVologda = PizzaVologda (
+        200.0, 123.8,
+        276.0, 342.0
+    )
+    var currentPizzaCity: PizzaCity
+    while (true) {
+        println("Добрый день! Выберите город")
+        println("1. Москва\n2. Санкт-Петербург\n3. Вологда\n\n0. Выход из программы")
+
+        currentPizzaCity = when (readln()) {
+            "1" -> pizzaMoscow
+            "2" -> pizzaPeter
+            "3" -> pizzaVologda
+            "0" -> break
+            else -> {
+                println("ERROR")
+                continue
+            } 1}
+        println("Выберите пиццу:")
+        println("1. Неаполитанская пицца\n2. Римская пицца\n3. Сицилийская пицца\n4. Тирольская пицца\n\n0. Показать статистику")
+
+        when (readln()) {
+            "1" -> {
+                currentPizzaCity.neapolitanPizzaSale()
+                selectAddService(currentPizzaCity) }
+            "2" -> {
+                currentPizzaCity.romanPizzaSale()
+                selectAddService(currentPizzaCity) }
+            "3" -> {
+                currentPizzaCity.sicilianPizzaSale()
+                selectAddService(currentPizzaCity) }
+            "4" -> {
+                currentPizzaCity.tyroleanPizzaSale()
+                selectAddService(currentPizzaCity) }
+            "0" ->  currentPizzaCity.showStatistics(getCityName(currentPizzaCity))
+            else -> {
+                println("ERROR")
+                exitProcess(1)
+            } } } }
+
+fun getCityName(currentPizzaCity: PizzaCity): String {
+    return when (currentPizzaCity) {
+        is PizzaMoscow -> "Moscow"
+        is PizzaPeter -> "Peter"
+        is PizzaVologda -> "Vologda"
+        else -> "Unknown"
+    } }
+
+fun selectAddService(currentPizzaCity: PizzaCity ){
+    when(currentPizzaCity){
+        is PizzaVologda -> {
+            currentPizzaCity.showCheckPhoto()
+            currentPizzaCity.SouseSale()
+        }
+        is PizzaMoscow -> currentPizzaCity.showCheckPhoto()
+    } }
---
+++ PizzaVologda.kt
@@ -0,0 +1,58 @@
+class PizzaVologda (
+    neapolitanPizzaPrice: Double, romanPizzaPrice: Double,
+    sicilianPizzaPrice: Double, tyroleanPizzaPrice: Double
+) : PizzaCity(
+    neapolitanPizzaPrice, romanPizzaPrice,
+    sicilianPizzaPrice, tyroleanPizzaPrice
+), CheckPhoto, Souse{
+    override fun SouseSale() {
+        print("Какой добавите соус?\n1.Карри\n2. Кисло-сладкий\n")
+        if(readln()== "1") {
+            souseCarryCount++
+            print("Стоимость соуса - 40 рублей")}
+        if(readln() =="2"){
+            souseKisloCount++
+            print("Стоимость соуса - 60 рублей") } }
+
+    override fun showCheckPhoto() {
+        println("У вас есть фотография чека?")
+        println("1. Да\n2. Нет")
+        if (readln()=="1") {
+            checkPhotoCount++
+            println("Вам будет скидка 50 рублей с покупки") } }
+    override fun neapolitanPizzaSale() {
+        println("Вы будете кофе?")
+        println("1. Да\n2. Нет")
+        if (readln() == "1"){
+            cofeeneapol++
+            println("С вас 200 рублей")}
+        neapolitanPizzaCount++
+        println("Спасибо за покупку неаполитанской пиццы в ") }
+
+
+    override fun romanPizzaSale() {
+        println("Вы будете кофе?")
+        println("1. Да\n2. Нет")
+        if (readln() == "1"){
+            cofeeroman++
+            println("С вас 200 рублей")}
+        romanPizzaCount++
+        println("Спасибо за покупку римской пиццы в ") }
+
+    override fun sicilianPizzaSale() {
+        println("Вы будете кофе?")
+        println("1. Да\n2. Нет")
+        if (readln() == "1"){
+            cofeesicili++
+            println("С вас 200 рублей")}
+        sicilianPizzaCount++
+        println("Спасибо за покупку сицилийской пиццы в ") }
+
+    override fun tyroleanPizzaSale() {
+        println("Вы будете кофе?")
+        println("1. Да\n2. Нет")
+        if (readln() == "1"){
+            cofeetyrolean++
+            println("С вас 200 рублей")}
+        tyroleanPizzaCount++
+        println("Спасибо за покупку тирольской пиццы в ") } }
